72142f4d15f4f57e2dc2186f715fb07197f2e2d7
Rehemah Gwokyalya <66667192+Rahmah-lab@users.noreply.github.com>
2024-07-28 18:06:46 +0300

    Update and rename 5.2.isoncorrectcosyra.sh to ISONcorrect.sh
    This is the script for ISONcorrect that correct for errors within gene families using the clustered reads. Run it individually for each barcode (update loop will be done soon)

--- ISONcorrect.sh
+++ ISONcorrect.sh
@@ -6,7 +6,7 @@
 #PBS -o /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/isoncorrect.out
 #PBS -e /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/isoncorrect.err
 #PBS -m abe
-#PBS -M jackiemaingih@gmail.com
+#PBS -M remysuffy@gmail.com
 #cd $PBS_O_WORKDIR
 
 #module load chpc/BIOMODULES
@@ -15,22 +15,15 @@
 #source activate /apps/chpc/bio/anaconda3-2020.02/envs/isoncorrect
 #conda activate isoncorrect
 
-#mkdir -p /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/sortme/sortmerna_outputs/insonclust_output/isoncorrect/output
-
-
-#for i in /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/sortme/sortmerna_outputs/*.unaligned.porechopped.fq;
-#do
-cd /home/manager/Desktop/Analysis/cosyra
+cd ~/RNAseq/fastq
 
 base=barcode01
 echo "Working with file $base"
 
-mkdir -p /home/manager/Desktop/Analysis/cosyra/sortme/sortmerna_output/insonclust_output/isoncorrect/output/$base
+mkdir -p ~/insonclust_output/isoncorrect/output/$base
 
-input=/home/manager/Desktop/Analysis/cosyra/sortme/sortmerna_output/insonclust_output/isoncorrect/input/${base}
+input=~/insonclust_output/isoncorrect/input/${base}
 
-output=/home/manager/Desktop/Analysis/cosyra/sortme/sortmerna_output/insonclust_output/isoncorrect/output/${base}
+output=~/insonclust_output/isoncorrect/output/${base}
 
 run_isoncorrect --fastq_folder $input --outfolder $output --exact_instance_limit 50 --max_seqs 1000 --k 9 --w 10 --xmin 14 --xmax 80 --T 0.1
-
-#done
