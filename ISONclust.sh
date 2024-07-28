3ae78a583adb342e8110eeb673649b9e48d48af0
Rehemah Gwokyalya <66667192+Rahmah-lab@users.noreply.github.com>
2024-07-28 18:03:35 +0300

    Update and rename 4.1.isonclustcosyra.sh to ISONclust.sh
    This is the script for the ISONclust step that clusters all reads into clusters of similar genes

--- ISONclust.sh
+++ ISONclust.sh
@@ -6,7 +6,7 @@
 #PBS -o /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/isonclust.out
 #PBS -e /mnt/lustre/users/jwahura/RNAseq/data/ovary_RNAseq/isonclust.err
 #PBS -m abe
-#PBS -M jackiemaingih@gmail.com
+#PBS -M remysuffy@gmail.com
 #cd $PBS_O_WORKDIR
 
 #module load chpc/BIOMODULES
@@ -15,18 +15,18 @@
 #source activate /apps/chpc/bio/anaconda3-2020.02/envs/isonclust
 #conda activate isonclust
 
-cd ~/Desktop/Analysis/cosyra/sortme/sortmerna_output
+cd ~/sortmerna_output
 
 mkdir -p insonclust_output
 
-for i in ~/Desktop/Analysis/cosyra/sortme/sortmerna_output/*.unaligned.porechopped.fq
+for i in ~/sortmerna_output/*.unaligned.porechopped.fq
 do
 
 base=$(basename $i .unaligned.porechopped.fq)
     echo "Working with file $base"
 
-i=~/Desktop/Analysis/cosyra/sortme/sortmerna_output/${base}.unaligned.porechopped.fq
-out=~/Desktop/Analysis/cosyra/sortme/sortmerna_outputs/insonclust_output/${base}
+i=~/sortmerna_output/${base}.unaligned.porechopped.fq
+out=~/sortmerna_outputs/insonclust_output/${base}
 
 isONclust --ont --fastq $i --outfolder $out
 done
